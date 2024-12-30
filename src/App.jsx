16a4040d25929774d08485a5a8c9b765a0ecafdd
avinash23em <ap23vi@gmail.com>
2024-12-30 13:36:48 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,21 @@ function App() {
      <div className='bg-blue-300'>hi</div>
 
 
-     <div className='flex justify-between'>
+      {/* <div className='flex justify-between'>
       <div>child 1</div>
       <div>child 2</div>
       <div>child 3</div>
+     </div> */}
+
+
+     {/*           implement grids    */}
+
+
+     <div className='grid grid-12'>
+      <div
+      className='bg-yellow-300 col-span-4' >child 1</div>
+      <div className='bg-red-300 col-span-4'>child 2</div>
+      <div className='bg-pink-300 col-span-4'>child 3</div>
      </div>
     </>
   )
